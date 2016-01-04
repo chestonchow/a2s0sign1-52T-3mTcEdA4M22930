d1437f51514cd85a94546e707e764bd525de7863
chestonchow <chestonchow2010@gmail.com>
2016-01-04 21:52:58 +1100

    Starting Point

---
+++ apps/js/addlocation.js
@@ -0,0 +1,91 @@
+//
+// MCD4290 T3 2015 Assignment 2 Weather Tracking app
+//
+// Team: 23
+//
+// Written by Yin Chun Chow
+//
+// addlocation.js
+//
+
+//setup before functions
+var typingTimer;                //timer identifier
+var doneTypingInterval = 2000;  //time in ms, 2 second for example
+var placeLat, placeLon;
+var locationObj;
+var googleAPIKey = "AIzaSyCB4CU_kuzDCCjRy6fe5xWKqVEA_kz3ojg";
+
+var key = "locations";
+
+
+//on keyup, start the countdown
+function keyUp(){
+	clearTimeout(typingTimer);
+  	typingTimer = setTimeout(doneTyping, doneTypingInterval);
+};
+
+//on keydown, clear the countdown 
+function keyDown() {
+	clearTimeout(typingTimer);
+};
+
+//user is "finished typing," do something
+function doneTyping () {
+	var address = document.getElementById("location").value;
+	var lowerCard = document.getElementById("img");
+	var width = lowerCard.offsetWidth-10;
+  	if (address){
+		console.log(address);
+
+		var oReq = new XMLHttpRequest();
+		oReq.addEventListener("load", reqListener);
+		oReq.open("GET", "https://maps.googleapis.com/maps/api/geocode/json?address="+address+"&key="+googleAPIKey);
+		oReq.send();
+		
+		function reqListener () {
+  			var jsonObj = JSON.parse(this.responseText);
+
+  			placeLat = jsonObj.results[0].geometry.location.lat;
+        	placeLon = jsonObj.results[0].geometry.location.lng;
+
+			while (lowerCard.firstChild) {
+  				lowerCard.removeChild(lowerCard.firstChild);
+			}
+			var img = new Image();
+    		img.src = "https://maps.googleapis.com/maps/api/staticmap?center="+placeLat+","+placeLon+"&zoom=15&size="+width+"x"+width+"&sensor=false&markers="+placeLat+","+placeLon;
+			lowerCard.appendChild(img);
+		}
+	}
+}
+
+function addPlace(){
+	var alias = document.getElementById("alias").value;
+	var alert = document.getElementById("alert");
+	if (alias && placeLat && placeLon){
+		alert.innerHTML = "";
+		if (typeof(Storage) !== "undefined"){
+			if (localStorage.getItem(key)){
+				locationObj = JSON.parse(localStorage.getItem(key));
+			} else {
+				locationObj = {
+					locations:[]
+				}
+			}
+			locationObj.locations.push({nickname:alias, lat:placeLat, lon:placeLon});
+			alert.innerHTML = "Added";
+			console.log(locationObj);
+			if (JSON.stringify(locationObj.locations[locationObj.locations.length - 1]) === JSON.stringify(locationObj.locations[locationObj.locations.length - 2])){
+				locationObj.locations.pop();
+				alert.innerHTML = "It's already there!!";
+			}
+			localStorage.setItem(key, JSON.stringify(locationObj));
+		} else {
+			alert.innerHTML = "localStorage is not supported by current browser.";
+		}
+	} else {
+		alert.innerHTML = "Please fill in all the fields!";
+	}
+				
+	//now clear memory
+	locationObj = {};
+}
---
+++ apps/js/weather.js
@@ -0,0 +1,155 @@
+//
+// MCD4290 T3 2015 Assignment 2 Weather Tracking app
+//
+// Team: 23
+//
+// Written by Yin Chun Chow
+//
+// weather.js
+//
+
+var key = "locations";
+var list = "", deList = "";
+var locationObj, weather;
+var forecastAPIKey = "52f2b4a4b97770fc067d41e6bae4a7fc";
+var currLoc = {
+	nickname:"Current Location",
+	lat:0,
+	lon:0
+	};
+
+document.getElementById("Alocation").innerHTML = "Current Location";
+
+if (navigator.geolocation) {
+	navigator.geolocation.getCurrentPosition(function (position){
+		currLoc.lat = position.coords.latitude;
+		currLoc.lon = position.coords.longitude;
+		showMap(currLoc);
+		getWeather(currLoc);}, null, {enableHighAccuracy: true});
+} else {
+	alert("Oops, no geolocation support");
+}
+
+
+
+
+if (typeof(Storage) !== "undefined"){
+	if (localStorage.getItem(key)){
+		locationObj = JSON.parse(localStorage.getItem(key));
+	} else {
+		locationObj = {
+			locations:[]
+		};
+	}
+	for (var i = 0; i < locationObj.locations.length; ++i) {
+		list += '<a class="mdl-navigation__link" onClick="changeActiveLocation(&quot;'+ locationObj.locations[i].nickname +'&quot;)">'+ locationObj.locations[i].nickname +'</a>';
+		deList += '<a class="mdl-navigation__link" onClick="deleteLocation(&quot;'+ locationObj.locations[i].nickname +'&quot;)">Delete '+ locationObj.locations[i].nickname +'</a>';
+	}
+
+	document.getElementById("list").innerHTML = list;
+	document.getElementById("deList").innerHTML = deList;
+
+} else {
+	console.log("localStorage is not supported by current browser.");
+}
+
+function changeActiveLocation(name){
+	var activeLocation;
+	document.getElementById("Alocation").innerHTML = name;
+	var firstCard = document.getElementById("img");
+	var width = firstCard.offsetWidth-10;
+	if (name === "Current Location"){
+		activeLocation = currLoc;
+	} else {
+		for (var i = 0; i < locationObj.locations.length; ++i) {
+			if (locationObj.locations[i].nickname === name){
+				activeLocation = locationObj.locations[i];
+				break;
+			}
+		}
+	}
+
+	showMap(activeLocation);
+	getWeather(activeLocation);
+	//Close Drawer
+	document.getElementsByClassName('mdl-layout__drawer')[0].classList.toggle("is-visible");
+	document.getElementsByClassName('mdl-layout__obfuscator')[0].classList.toggle("is-visible");
+}
+
+function deleteLocation(name){
+	if (typeof(Storage) !== "undefined"){
+			if (localStorage.getItem(key)){
+				locationObj = JSON.parse(localStorage.getItem(key));
+				for (var i = 0; i < locationObj.locations.length; ++i) {
+					if (locationObj.locations[i].nickname === name){
+						locationObj.locations.splice(i, 1);
+					}
+				}
+			}
+			console.log(locationObj);
+			localStorage.setItem(key, JSON.stringify(locationObj));
+		}
+
+	//reload page
+	document.location.reload(true);
+}
+
+function showMap(location){
+	var firstCard = document.getElementById("img");
+	var width = firstCard.offsetWidth-10;
+	while (firstCard.firstChild) {
+		firstCard.removeChild(firstCard.firstChild);
+	}
+	var img = new Image();
+	img.src = "https://maps.googleapis.com/maps/api/staticmap?center="+location.lat+","+location.lon+"&zoom=15&size="+width+"x"+width+"&sensor=false&markers="+location.lat+","+location.lon;
+	firstCard.appendChild(img);
+}
+
+function getWeather(location){
+	var secondCard = document.getElementById("curr"), thirdCard = document.getElementById("fore");
+	
+	//weather = JSON.parse(localStorage.getItem("weather"));
+
+	function doThis(data){
+   		console.log(data);
+		weather = data;
+	}
+
+	var script = document.createElement('script');
+	script.src = "https://api.forecast.io/forecast/"+forecastAPIKey+"/"+location.lat+","+location.lon+"?units=ca?callback=doThis";
+	console.log(script.src);
+	document.body.appendChild(script);
+
+	/*var oReq = new XMLHttpRequest();
+	oReq.open("GET","https://api.forecast.io/forecast/"+forecastAPIKey+"/"+location.lat+","+location.lon+"?units=ca");
+	oReq.addEventListener("load", function(){console.log(this.responseText);weather = JSON.parse(this.responseText);});
+	oReq.send();*/
+
+	//Current Weather
+
+	//Weather Forecast
+	var row1 = "<thead><tr><th>7 day Weather Forecast</th>",
+	row2 = "<tbody><tr><td>Min Temp (&ordm;C)</td>",
+	row3 = "<tr><td>Max Temp. (&ordm;C)</td>",
+	row4 = "<tr><td></td>";
+
+	var weekday = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+	
+	for (var i = 0; i < 7; ++i)
+	{
+		var date = new Date();
+		date.setTime((weather.daily.data[i].time+weather.offset*3600+date.getTimezoneOffset()*60)*1000);
+		var num = date.getDay();
+		var nameOfDay = weekday[num];
+		var minTemp = weather.daily.data[i].temperatureMin.toFixed(0);
+		var maxTemp = weather.daily.data[i].temperatureMax.toFixed(0);
+		var icon = weather.daily.data[i].icon;
+
+		row1 += "<th>" + nameOfDay + "</th>";
+		row2 += "<td>" + minTemp + "</td>";
+		row3 += "<td>" + maxTemp + "</td>";
+		row4 += "<td><img src='images/icons/"+icon+".png'></td>";
+	}
+
+	thirdCard.innerHTML=row1+"</tr></thead>"+row2+"</tr>"+row3+"</tr>"+row4+"</tr></tbody>";
+}
