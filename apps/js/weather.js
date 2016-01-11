8bb4afa2f6a8f9f89431e326567bd5826f564bf1
jluo63 <chestonchow@Chestons-MacBook-Pro.local>
2016-01-11 22:59:34 +1100

    average temp last 4 days

--- apps/js/weather.js
+++ apps/js/weather.js
@@ -12,6 +12,7 @@ var key = "locations";
 var list = "", deList = "";
 var locationObj;
 var forecastAPIKey = "52f2b4a4b97770fc067d41e6bae4a7fc";
+var firstDay, secondDay, thirdDay, fourthDay;
 var currLoc = {
 	nickname:"Current Location",
 	lat:0,
@@ -29,9 +30,6 @@ if (navigator.geolocation) {
 } else {
 	alert("Oops, no geolocation support");
 }
-
-
-
 
 if (typeof(Storage) !== "undefined"){
 	if (localStorage.getItem(key)){
@@ -52,6 +50,10 @@ if (typeof(Storage) !== "undefined"){
 } else {
 	console.log("localStorage is not supported by current browser.");
 }
+
+
+
+
 
 function changeActiveLocation(name){
 	var activeLocation;
@@ -86,7 +88,6 @@ function deleteLocation(name){
 					}
 				}
 			}
-			console.log(locationObj);
 			localStorage.setItem(key, JSON.stringify(locationObj));
 		}
 
@@ -106,12 +107,51 @@ function showMap(location){
 }
 
 function requestWeather(location){
-	// Make the request
+	// Make request
 	var script = document.createElement('script');
 	script.src = "https://api.forecast.io/forecast/"+forecastAPIKey+"/"+location.lat+","+location.lon+"?units=ca&callback=printWeather";
 	console.log(script.src);
 	document.body.appendChild(script);
+
+	var today = new Date();
+	var y = today.getFullYear();
+	var m = today.getMonth();
+	var d = today.getDate();
+	var day1 = y+"-"+(m<9?"0":"")+(m+1)+"-"+(d<14?"0":"")+(d-4);
+	var day2 = y+"-"+(m<9?"0":"")+(m+1)+"-"+(d<13?"0":"")+(d-3);
+	var day3 = y+"-"+(m<9?"0":"")+(m+1)+"-"+(d<12?"0":"")+(d-2);
+	var day4 = y+"-"+(m<9?"0":"")+(m+1)+"-"+(d<11?"0":"")+(d-1);
+
+	var script4 = document.createElement('script');
+	script4.src = "https://api.forecast.io/forecast/"+forecastAPIKey+"/"+location.lat+","+location.lon+","+day1+"T12:00:00?units=ca&callback=fourDaysAgo";
+	document.body.appendChild(script4);
+
+	var script3 = document.createElement('script');
+	script3.src = "https://api.forecast.io/forecast/"+forecastAPIKey+"/"+location.lat+","+location.lon+","+day2+"T12:00:00?units=ca&callback=threeDaysAgo";
+	document.body.appendChild(script3);
+
+	var script2 = document.createElement('script');
+	script2.src = "https://api.forecast.io/forecast/"+forecastAPIKey+"/"+location.lat+","+location.lon+","+day3+"T12:00:00?units=ca&callback=twoDaysAgo";
+	document.body.appendChild(script2);
+
+	var script1 = document.createElement('script');
+	script1.src = "https://api.forecast.io/forecast/"+forecastAPIKey+"/"+location.lat+","+location.lon+","+day4+"T12:00:00?units=ca&callback=yesterday";
+	document.body.appendChild(script1);
 }
+
+function fourDaysAgo(d){
+	firstDay = d.currently.temperature;
+}
+function threeDaysAgo(d){
+	secondDay = d.currently.temperature;
+}
+function twoDaysAgo(d){
+	thirdDay = d.currently.temperature;
+}
+function yesterday(d){
+	fourthDay = d.currently.temperature;
+}
+
 
 function printWeather(weather) {
 	console.log(weather);
@@ -123,23 +163,23 @@ function printWeather(weather) {
 	var txt = weather.currently.summary;
 	var hum = weather.currently.humidity*100;
 	var wind = Math.round(weather.currently.windSpeed);
-	var last4 = currTemp+(Math.random() > 0.5 ? 1 : -1)*Math.floor(Math.random() * 5);//(here need to change)
+	var last4 = Math.round((firstDay+secondDay+thirdDay+fourthDay)/4);
 
 	secondCard.innerHTML = "<p><img src='images/icons/"+icon+".png' class='currIcon'>&nbsp;&nbsp;<span class='currTemp'>"+currTemp+"&ordm;C</span><br>"+txt+"</p><p><br>Humidity is "+hum+"%<br>Wind Speed is "+wind+"km/h<br>Average midday temperature of last 4 days is "+last4+"&ordm;C</p>";
 	//Weather Forecast
 	var row1 = "<thead><tr><th>7 day Weather Forecast</th>",
-	row2 = "<tbody><tr><td>Min Temp (&ordm;C)</td>",
+	row2 = "<tbody><tr><td>Min Temp. (&ordm;C)</td>",
 	row3 = "<tr><td>Max Temp. (&ordm;C)</td>",
 	row4 = "<tr><td></td>";
 
 	var weekday = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
 	
-	for (var i = 0; i < 7; ++i)
+	for (var i = 1; i < 8; ++i)
 	{
 		var date = new Date();
 		date.setTime((weather.daily.data[i].time+weather.offset*3600+date.getTimezoneOffset()*60)*1000);
 		var num = date.getDay();
-		var nameOfDay = (i == 0 ? "To<br>day" : weekday[num]);
+		var nameOfDay = (i == 7 ? "Next<br>" : "") + weekday[num];
 		var minTemp = Math.round(weather.daily.data[i].temperatureMin);
 		var maxTemp = Math.round(weather.daily.data[i].temperatureMax);
 		var icon = weather.daily.data[i].icon;
